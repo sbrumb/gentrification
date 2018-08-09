8476aadcab1753fb08044d68bdc912124f398677
Stephen Brumbaugh <stephen.brumbaugh@gmail.com>
2018-08-09 12:26:19 -0400

    Test with 2010 ACS instead of 2000 Census

--- classify tracts.R
+++ classify tracts.R
@@ -29,6 +29,7 @@ hh_inc_2016 <- reduce(
   rbind
 )
 
+
 hh_inc_2016_msa_src <- get_acs(
   geography = "metropolitan statistical area/micropolitan statistical area",
   variables = c("B01003_001", "B19013_001"), year = 2016)
@@ -43,6 +44,18 @@ hh_inc_2016_msa <- hh_inc_2016_msa_src %>%
   head(100) %>%
   rename(msa = GEOID)
 
+hh_inc_2010 <- reduce(
+  map(us, function(x) {
+    get_acs(geography = "tract", variables = c("B19013_001"),
+            state = x, year = 2010)
+  }),
+  rbind
+)
+
+hh_inc_2010 <- hh_inc_2010 %>%
+  rename(estimate_2010 = estimate) %>%
+  select(-NAME, -variable, -moe)
+
 metros <- core_based_statistical_areas(cb = TRUE)
 metros_join <- metros %>%
   select(metro_name = NAME,
@@ -53,7 +66,7 @@ hh_inc_2016_with <- st_join(hh_inc_2016, metros_join, join = st_within,
                       left = FALSE)
 
 hh_inc_2016_with2 <- hh_inc_2016_with %>%
-  spread(variable, estimate_2016) %>%
+  spread(variable, estimate) %>%
   rename(estimate_2016 = B19013_001,
          population_2016 = B01003_001) %>%
   group_by(GEOID) %>%
@@ -63,15 +76,28 @@ hh_inc_2016_with2 <- hh_inc_2016_with %>%
             estimate_2016 = estimate_2016[which(!is.na(estimate_2016))[1]],
             population_2016 = population_2016[which(!is.na(population_2016))[1]])
 
+# hh_inc <- hh_inc_2016_with2 %>%
+#   left_join(hh_inc_2016_msa, by = "msa") %>%
+#   left_join(hh_inc_2000_nospatial, by = "GEOID")
+
 hh_inc <- hh_inc_2016_with2 %>%
   left_join(hh_inc_2016_msa, by = "msa") %>%
-  left_join(hh_inc_2000_nospatial, by = "GEOID")
+  left_join(hh_inc_2010, by = "GEOID")
+
+
+# hh_inc_allmetros <- hh_inc %>%
+#   group_by(metro_name) %>%
+#   mutate(decile_2000 = ntile(estimate_2000, 10),
+#          decile_2016 = ntile(estimate_2016, 10),
+#          change_d = decile_2016 - decile_2000,
+#          change = cut(change_d, breaks = c(-Inf, -2, 2, Inf))) %>%
+#   ungroup()
 
 hh_inc_allmetros <- hh_inc %>%
   group_by(metro_name) %>%
-  mutate(decile_2000 = ntile(estimate_2000, 10),
+  mutate(decile_2010 = ntile(estimate_2010, 10),
          decile_2016 = ntile(estimate_2016, 10),
-         change_d = decile_2016 - decile_2000,
+         change_d = decile_2016 - decile_2010,
          change = cut(change_d, breaks = c(-Inf, -2, 2, Inf))) %>%
   ungroup()
 
@@ -80,12 +106,22 @@ hh_inc_allmetros$change <- hh_inc_allmetros$change %>%
                                    "Stable" = "(-2,2]",
                                    "Gentrifying" = "(2, Inf]")
 
+# hh_inc_allmetros <- hh_inc_allmetros %>%
+#   mutate(change = ifelse(decile_2000 > 4 & change == "Gentrifying",
+#                          "Upgrading", as.character(change)),
+#          change = ifelse(decile_2016 > 4 & change == "Declining",
+#                          "Downgrading", as.character(change)),
+#          change = ifelse(decile_2000 <= 4 & decile_2016 <= 4 &
+#                            change == "Stable",
+#                          "Stable low-income", as.character(change)),
+#          change = as.factor(change))
+
 hh_inc_allmetros <- hh_inc_allmetros %>%
-  mutate(change = ifelse(decile_2000 > 4 & change == "Gentrifying",
+  mutate(change = ifelse(decile_2010 > 4 & change == "Gentrifying",
                          "Upgrading", as.character(change)),
          change = ifelse(decile_2016 > 4 & change == "Declining",
                          "Downgrading", as.character(change)),
-         change = ifelse(decile_2000 <= 4 & decile_2016 <= 4 &
+         change = ifelse(decile_2010 <= 4 & decile_2016 <= 4 &
                            change == "Stable",
                          "Stable low-income", as.character(change)),
          change = as.factor(change))
